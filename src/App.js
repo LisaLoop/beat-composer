9ac2aff3a5ada3159ce220294ca319db1ef683cf
L L <ll@Ls-MacBook-Pro.local>
2020-05-02 20:16:14 -0700

    adds recording, limits track to 2 seconds

--- src/App.js
+++ src/App.js
@@ -1,14 +1,79 @@
 import React from 'react';
-import {playAudioFromDrumPad, playAudioFromKeyboard, DrumPad} from './drum-machine'
+import {playAudioFromDrumPad, playAudioFromKeyboard, DrumPad, RecordButton, PlayButton, playAudio} from './drum-machine'
 import './App.css';
 
 const App = ({ drumData }) => {
-  console.log(JSON.stringify(drumData));
+  const [isRecording, setIsRecording] = React.useState(false);
+  const [start, setStart] = React.useState(null);
+  const [events, setEvents] = React.useState([]);
+  
+  const getIsRecording = () => {
+    return isRecording
+  }
+
+  const getEvents = () => {
+    return events;
+  }
+  const play = (events) => {
+    console.log("events: ", events);
+    // bpm is 120
+    // Quantizing 
+    let newEvents = [];
+    events.map((event) => {
+      let t = (event.time - start) // 
+      let remainder = t % 125; // User can configure 2 seconds 
+      event.quantizedEvent = t - remainder; // 2000/16 (2 seconds, 16 bit track)
+      newEvents.push(event)
+      
+    });
+
+    newEvents.map((event) => {
+      // let timeInFuture = event.time - start;
+      let timeInFuture = event.quantizedEvent;
+      console.log("timeInFuture: ", timeInFuture);
+      // truncate after 2 seconds
+      if(timeInFuture > 2000) {
+        return;
+      }
+      // TODO: clear timeout to pause the song
+      setTimeout(() => {
+        console.log(event);
+        // TODO: Download the sounds so that it doesn't break when the url changes
+        playAudio(event.data.url)
+      },timeInFuture);
+    })
+
+    setTimeout(() => play(events),2000)
+  }
+  
+  const addEvent = (data) => {
+    events.push({time: Date.now(), data: data})
+    // console.log("events: ", events);
+    // console.log("pitch: ", pitch);
+    setEvents(events)
+  }
+    const toggleRecording = () => {
+        if(isRecording){
+            setIsRecording(false)
+            console.log("events: ", events);
+        } else {
+            setIsRecording(true)
+            setEvents([]);
+            setStart(Date.now())
+        }
+    }
   return (
     <main onKeyPress={e => { console.log(e.key); playAudioFromKeyboard(drumData, e.key); }}>
       <div className="pad">
       {drumData
-        .map(data => <DrumPad key={data.id} data={data} onHit={playAudioFromDrumPad} />)}
+        .map(data => <DrumPad getIsRecording={getIsRecording}
+                              addEventHandler={addEvent} 
+                              key={data.id} data={data} 
+                              onHit={playAudioFromDrumPad} />)}
+      </div>
+      <div style={{width: "100%", textAlign:"center"}}>
+        <RecordButton getIsRecording={getIsRecording} onRecord={toggleRecording}/>
+        <PlayButton onPlay={() => play(getEvents())}/>
       </div>
     </main>
   );
--- src/drum-machine.js
+++ src/drum-machine.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+// 120 bpm = 2hrtz
+
 export const playAudio = (url) => {
   let audio = document.createElement("audio");
-  audio.src = url;
+  audio.src = url; 
   const playAudio = () => {
     audio.play();
     audio.removeEventListener("canplay", playAudio);
@@ -17,7 +19,102 @@ export const playAudioFromKeyboard = (drums, code) => {
   if (data) playAudio(data.url);
 };
 
-export const playAudioFromDrumPad = ({ url }) => playAudio(url);
+export const playAudioFromDrumPad = ({ url }) => {
+    playAudio(url)
+};
 
-export const DrumPad = ({ data, onHit }) =>
-  <button className="key" onClick={() => onHit(data)}>{data.id}</button>;
+export const DrumPad = ({ data, onHit, getIsRecording, addEventHandler}) => {
+    const clickHandler = () => {
+        console.log("getIsRecording:", getIsRecording());
+        if(getIsRecording()){
+            addEventHandler(data)        
+        }
+        onHit(data)
+    }
+    return <button className="key" onClick={clickHandler}>{data.id}</button>;
+}
+
+
+export const RecordButton = ({onRecord, getIsRecording}) => {
+    return (
+        <button 
+        className={`record-button ${getIsRecording() ? 'recording-style':''}`}
+        onClick={onRecord}> </button>
+    )
+}
+
+export const PlayButton = ({onPlay}) => {
+    return (
+        <button className="play-button" onClick={onPlay}>⏯</button>
+    )
+}
+
+/*** 
+ * 	sounds = []
+
+
+	sounds[0] = {"name":"drum1","file":"file.mp3"}
+	sounds[1] = {"name":"drum1","file":"file.mp3"}
+	sounds[2] = {"name":"drum1","file":"file.mp3"}
+	sounds[3] = {"name":"drum1","file":"file.mp3"}
+
+
+	//4
+	modes = []
+	modes[0] = [0,1,2,3];
+	modes[1] = [4,5,6,7];
+	modes[2] = [8,9,10,11];
+
+	modes[3] = [0,1,6,9];
+
+	currentsoundset = [0,1,2,3];
+
+	isRecording = 0
+
+	record:
+		if isRecording is 0:
+			st = now
+			song = []
+		else:
+			end =  now
+			beat = ( 1000 / 60 ) * 16
+
+			time2offset():
+				for all events:
+					subtract start date
+	
+				=>
+			offset2perfectoffset():
+				for x in event:
+					event.time = quantize(event.time) round 
+
+			quantizedSong = out ^
+
+	button.onClick(b):
+		if isRecording:
+			song.push({time:now, pitch: currentsoundset[b] })
+		playSound()
+
+	right.onclick:
+		currentmode ++ ;
+		if currentmode > 3
+			currentmode = 0
+		currentsoundset = modes[currentmode];
+
+	right.onclick:
+		currentmode -- ;
+		if currentmode < 0
+			currentmode = 3
+		currentsoundset = modes[currentmode];
+
+	play:
+		ss = now 
+		for each event in quantizedSong:
+			ev.time = event.time + ss
+		
+		for x in :
+			setTimeout("playSound(x.pitch)",x.time)
+
+	download:
+		convert to mp3 download.
+*/
