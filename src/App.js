037535bab2e2b8a965298a910bc8e67f37ddcbde
L L <ll@Ls-MacBook-Pro.local>
2020-05-08 17:35:50 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import {playAudioFromDrumPad, playAudioFromKeyboard, DrumPad, RecordButton, PlayButton, playAudio} from './drum-machine'
 import './App.css';
 
+/* TODO
+- Styling 
+- Show recording bar
+- Ability to stop/pause
+- Add step sequencer to allow user to remove notes
+*/
+
 const App = ({ drumData }) => {
   const [isRecording, setIsRecording] = React.useState(false);
   const [start, setStart] = React.useState(null);
@@ -15,14 +22,24 @@ const App = ({ drumData }) => {
     return events;
   }
   const play = (events) => {
+    if(events.length === 0){
+      setTimeout(() => play(events),2000)
+      return;
+    }
     console.log("events: ", events);
     // bpm is 120
     // Quantizing 
     let newEvents = [];
+    // let songStartDelay = start - events[0].time;
     events.map((event) => {
-      let t = (event.time - start) // 
+      // allows the user time to play the first sound
+      let st = events[0].time;
+      let t = (event.time - st) // 
       let remainder = t % 125; // User can configure 2 seconds 
       event.quantizedEvent = t - remainder; // 2000/16 (2 seconds, 16 bit track)
+      // Continue recording during playback
+      // TODO:let user configure bpm 120/140...etc. 
+      event.quantizedEvent = event.quantizedEvent % 2000; 
       newEvents.push(event)
       
     });
@@ -32,9 +49,9 @@ const App = ({ drumData }) => {
       let timeInFuture = event.quantizedEvent;
       console.log("timeInFuture: ", timeInFuture);
       // truncate after 2 seconds
-      if(timeInFuture > 2000) {
-        return;
-      }
+      // if(timeInFuture > (2000 + songStartDelay)) { // not perfect, but it will give user more time to record
+      //   return;
+      // }
       // TODO: clear timeout to pause the song
       setTimeout(() => {
         console.log(event);
@@ -52,16 +69,17 @@ const App = ({ drumData }) => {
     // console.log("pitch: ", pitch);
     setEvents(events)
   }
-    const toggleRecording = () => {
-        if(isRecording){
-            setIsRecording(false)
-            console.log("events: ", events);
-        } else {
-            setIsRecording(true)
-            setEvents([]);
-            setStart(Date.now())
-        }
-    }
+  const toggleRecording = () => {
+      if(isRecording){
+          setIsRecording(false)
+          console.log("events: ", events);
+      } else {
+          setIsRecording(true)
+          setEvents([]);
+          // TODO: as soon as user hits first note, start the 2 second delay
+          setStart(Date.now())
+      }
+  }
   return (
     <main onKeyPress={e => { console.log(e.key); playAudioFromKeyboard(drumData, e.key); }}>
       <div className="pad">
